2dc26a67f9e0aab7a79b0cced07d7946241487f0
EC2 Default User <ec2-user@ip-172-31-37-125.ec2.internal>
2018-11-13 21:47:49 +0000

    Post and Get function

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 var mongoose = require('mongoose'); //Adds mongoose as a usable dependency
 
 mongoose.connect('mongodb://localhost/commentDB',{ useNewUrlParser: true });
-var commentSchema = mongoose.Schema({ //Defines the Schema for this database
+var reviewSchema = mongoose.Schema({ //Defines the Schema for this database
     Name: String,
     Comment: String
 });
-var Comment = mongoose.model('Comment', commentSchema); //Makes an object from that schema as a model
+var Review = mongoose.model('Review', reviewSchema); //Makes an object from that schema as a model
 var db = mongoose.connection; //Saves the connection as a variable to use
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
@@ -15,8 +15,37 @@ db.once('open', function() {
 });
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/comment', function(req, res, next) {
+    console.log("POST comment route"); 
+    console.log(req.body);
+    var newcomment = new Review(req.body); 
+    console.log(newcomment); 
+    newcomment.save(function(err, post) { 
+      if(err) {console.log("Save error");}
+      else {
+        console.log("Save worked");
+        console.log(post);
+        res.sendStatus(200);  
+      }
+    });
+});
+
+router.get('/comment', function(req, res, next) {
+    console.log("In the GET route?");
+    console.log(req.query);
+    var searchName = req.query["q"];
+    console.log(searchName);
+    var obj = {};
+    if(searchName) {
+      obj = {Name: searchName};
+    }
+    Review.find(obj,function(err,reviewList) { //Calls the find() method on your database
+      if (err) return console.error(err); //If there's an error, print it out
+      else {
+        console.log(reviewList); //Otherwise console log the comments you found
+        res.json(reviewList); //Then send the comments
+      }
+    });
 });
 
 module.exports = router;
