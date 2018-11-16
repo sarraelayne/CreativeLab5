2d94c42735da36da8e732a0c1f6a6a44ed11a564
EC2 Default User <ec2-user@ip-172-31-37-125.ec2.internal>
2018-11-16 06:49:16 +0000

    CSS done, debug repeat

--- public/javascripts/ratings.js
+++ public/javascripts/ratings.js
@@ -24,30 +24,32 @@ $(document).ready(function() {
   $("#getComments").click(function() {
     $.getJSON('comment', function(data) {
       console.log(data);
-      var everything = "<h1>Comments Matrix</h1> <br>";
+      var everything = "";
       for (var comment in data) {
         var com = data[comment];
+        //if nothing new, then don't call this function? 
         if (Dict[com.Name]) {
           Dict[com.Name].push(com);
+          console.log("pushed");
         }
         else {
           Dict[com.Name] = [com];
         }
       }
-      console.log(Dict);
+      console.log("dict: ", Dict);
       for (var comment in Dict) {
         console.log("comment", comment);
         com = Dict[comment];
         console.log(com);
-        everything += "Name: " + comment;
+        everything += "<div class=\"commentCard\">" + "Name: " + comment + "<br>";
         for (var data in com) {
           console.log(data);
           var dat = com[data];
-          everything += " -- Comment: " + dat.Comment + "<br>";
+          everything += "<br>" + "Comment: " + dat.Comment + "<br>";
           everything += "Image Link: " + dat.Image + "<br>";
           everything += "Type of Food: " + dat.Type + "<br>";
         }
-        
+        everything += "</div><br>";
       }
       $("#comments").html(everything);
     });
@@ -71,13 +73,15 @@ $(document).ready(function() {
     console.log(URL);
     $.getJSON(URL, function(data) {
       console.log(data);
-      var userCom = "<h1>Comments Matrix</h1> <br>";
-      userCom += name;
+      var userCom = "";
+      userCom += "<h2>" + name + "</h2>";
       for (var comment in data) {
         var com = data[comment];
+        userCom += "<div class=\"commentCard\">";
         userCom += "Comment: " + com.Comment + "<br>";
         userCom += "Image Link: " + com.Image + "<br>";
         userCom += "Type of Food: " + com.Type + "<br>";
+        userCom += "</div><br>";
       }
       $("#comments").html(userCom);
     });
